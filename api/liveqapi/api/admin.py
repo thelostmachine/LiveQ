117a955f01aa25f60193fb137d88aadd467667e4
eriks <erikswanson@tamu.edu>
2020-02-24 15:22:41 -0600

    register models

--- api/liveqapi/api/admin.py
+++ api/liveqapi/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Room, Queue, Song, Service
 # Register your models here.
+admin.site.register(Room)
+admin.site.register(Queue)
+admin.site.register(Song)
+admin.site.register(Service)
