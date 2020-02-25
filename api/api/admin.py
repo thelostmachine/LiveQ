a76236d162008152b7dc3f1ce538af803f859c27
eriks <erikswanson@tamu.edu>
2020-02-24 19:47:23 -0600

    Update admin.py

--- api/api/admin.py
+++ api/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Room, Queue, Song, Service
+from .models import Room, Guest, Song, Service
 # Register your models here.
 admin.site.register(Room)
 admin.site.register(Guest)
