dc0e0ae33e92cfea0015092a96d2d86255fd971c
eriks <erikswanson@tamu.edu>
2020-02-24 19:44:18 -0600

    updated model

--- api/api/models.py
+++ api/api/models.py
@@ -2,33 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+class Guest(models.Model):
+    room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 # Room DB model
 class Room(models.Model):
     room_key = models.CharField(max_length=30)
     room_name = models.CharField(max_length=30)
     host = models.OneToOneField(User, on_delete=models.CASCADE)
-    guests = models.ManyToManyField(User)
-    queue = models.OneToOneField('Queue', on_delete=models.CASCADE)
-    services = models.ManyToManyField('Service')
-
-# Queue DB model
-class Queue(models.Model):
-    songs = models.ManyToManyField('Song')
-
+   
 # Song DB model
 class Song(models.Model):
+    room = models.ForeignKey(Room, on_delet=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
     song_id = models.CharField(max_length=30)
     service = models.ForeignKey('Service', on_delete=models.CASCADE)
 
+    def save(self):
+        super(Song, self).save()
+
 # Service DB model
 class Service(models.Model):
-    class ServiceType(models.TextChoices):
-        SPOTIFY = 'SP'
-        SOUNDCLOUD = 'SC'
-    
+    class ServiceType(models.IntegerChoices):
+        SPOTIFY = '0'
+        SOUNDCLOUD = '1'
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     api_key = models.CharField(max_length=30)
-    service_type = models.CharField(
-        max_length=2,
-        choices=ServiceType.choices,
-        default=ServiceType.SPOTIFY)
+    service_type = models.IntegerField(choices=ServiceType.choices)
  
