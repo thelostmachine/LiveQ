eee4b0a7baeacd45e2a330cf8bae5f57c28e3e4e
eriks <erikswanson@tamu.edu>
2020-03-03 18:32:38 -0600

    admin import

--- api/liveqapi/urls.py
+++ api/liveqapi/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
+from django.contrib import admin
 from rest_framework import routers
 from api import views
 
