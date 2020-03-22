516466467f4faffaf8d4c4342a82fe67c9e60617
eriks <erikswanson@tamu.edu>
2020-03-22 18:53:30 -0500

    create, join room

--- api/lqs.py
+++ api/lqs.py
@@ -2,7 +2,40 @@ import grpc
 import interface_pb2
 import interface_pb2_grpc
 import server_resources
+import uuid
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
 class LiveQSevicer(interface_pb2_grpc.LiveQServicer):
     def __init__(self):
+        self.db = server_resources.RoomDB()
 
     def CreateRoom(self, request, context):
+        reply_info = self.db.AddRoom(request.room_name)
+        return interface_pb2.CreateReply(room_key=reply_info[0], host_id=reply_info[1])
+
+    def JoinRoom(self, request, context):
+        reply_info = self.db.JoinRoom(request.room_key)
+        return interface_pb2.JoinReply(guest_id=reply_info)
+
+    def UpdateQueue(self, request, context):
+
+    def AddSong(self, request, context):
+
+    def DeleteSong(self, request, context):
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    interface_pb2_grpc.add_LiveQServicer_to_server(LiveQSevicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
--- api/server_resources.py
+++ api/server_resources.py
@@ -1,11 +1,41 @@
+import uuid
+
+
+
+
+class RoomDB:
+    def __init__(self):
+        self.rooms = {}
+    
+    def generate_key(self):
+        while True:
+            lettersAndDigits = string.ascii_letters + string.digits
+            key = ''.join(random.choice(lettersAndDigits) for i in range(8))
+            if key not in self.rooms.keys():
+                return key
+    
+    def AddRoom(self, name):
+        new_room = Room(name, self.generate_key())
+        self.rooms[new_room.room_key] = new_room
+        return [new_room.room_key, str(new_room.host_id)]
+    
+    def JoinRoom(self, key):
+        if key in self.rooms.keys():
+            return self.rooms[key].AddGuest()
 
 class Room:
-    def __init__(self, name, host_id):
+    def __init__(self, name, key):
         self.name = name
-        self.host_id = host_id
-
-class Queue:
-    def __init__(self, ):
+        self.room_key = key
+        self.host_id = uuid.uuid1()
+        self.guest_ids = []
+        self.q = queue.Queue()
+    def AddGuest(self):
+        guest_id = uuid.uuid1()
+        self.guest_ids.append(guest_id)
+        return guest_id
 
 class Song:
-    def __init__(self, ):
+    def __init__(self, song_id, service_id):
+        self.song_id = song_id
+        self.service_id = service_id
