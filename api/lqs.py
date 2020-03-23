bab729f9758c2a43f637e01581079cd4fe020fbd
eriks <erikswanson@tamu.edu>
2020-03-23 13:00:06 -0500

    queue operations

--- api/lqs.py
+++ api/lqs.py
@@ -10,7 +10,7 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 class LiveQSevicer(interface_pb2_grpc.LiveQServicer):
     def __init__(self):
         self.db = server_resources.RoomDB()
-
+    
     def CreateRoom(self, request, context):
         reply_info = self.db.AddRoom(request.room_name)
         return interface_pb2.CreateReply(room_key=reply_info[0], host_id=reply_info[1])
@@ -20,11 +20,22 @@ class LiveQSevicer(interface_pb2_grpc.LiveQServicer):
         return interface_pb2.JoinReply(guest_id=reply_info)
 
     def UpdateQueue(self, request, context):
+        q = self.db[request.room_key].q
+        for song in q:
+            yield song
 
     def AddSong(self, request, context):
+        self.db.AddSong(request.room_key, interface_pb2.QueueReply(song_id=request.song_id, service_id=request.service_id))
+        q = self.db[request.room_key].q
+        for song in q:
+            yield song
 
     def DeleteSong(self, request, context):
-
+        self.db.DeleteSong(request.room_key, interface_pb2.QueueReply(song_id=request.song_id, service_id=request.service_id))
+        q = self.db[request.room_key].q
+        for song in q:
+            yield song
+            
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     interface_pb2_grpc.add_LiveQServicer_to_server(LiveQSevicer(), server)
--- api/server_resources.py
+++ api/server_resources.py
@@ -2,12 +2,13 @@ import uuid
 import string
 import random
 import queue
+import asyncio
 
 
 class RoomDB:
     def __init__(self):
         self.rooms = {}
-    
+     
     def generate_key(self):
         while True:
             lettersAndDigits = string.ascii_letters + string.digits
@@ -23,6 +24,14 @@ class RoomDB:
     def JoinRoom(self, key):
         if key in self.rooms.keys():
             return self.rooms[key].AddGuest()
+    
+    def AddSong(self, key, song):
+        if key in self.rooms.keys():
+            self.rooms[key].AddSongToQ(song)
+    
+    def DeleteSong(self, key, song):
+        if key in self.rooms.keys():
+            self.rooms[key].DelSongQ(song)
 
 class Room:
     def __init__(self, name, key):
@@ -30,14 +39,19 @@ class Room:
         self.room_key = key
         self.host_id = uuid.uuid1()
         self.guest_ids = []
-        self.q = queue.Queue()
+        self.q = []
 
     def AddGuest(self):
         guest_id = uuid.uuid1()
         self.guest_ids.append(guest_id)
         return guest_id
 
-class Song:
-    def __init__(self, song_id, service_id):
-        self.song_id = song_id
-        self.service_id = service_id
+    def AddSongToQ(self, song):
+        q.append(song)
+    
+    def DelSongQ(self, song):
+        for item in self.q:
+            if item == song :
+                del item
+
+  
