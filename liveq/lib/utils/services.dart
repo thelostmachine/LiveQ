bb678564690011501d6a040e203ac7be2df81ffc
Shaheer Mirza <shaheermirzacs@gmail.com>
2020-04-05 17:40:37 -0500

    simplified search function

--- liveq/lib/utils/services.dart
+++ liveq/lib/utils/services.dart
@@ -1,5 +1,3 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 import 'package:spotify_sdk/spotify_sdk.dart';
@@ -45,101 +43,49 @@ class Spotify extends Service {
   @override
   Future<bool> connect() async {
 
-    // Connect to Spotify only if on mobile
-    if (kIsWeb) {
-      var credentials = SpotifyApiCredentials(clientId, clientSecret);
-      spotifyWebApi = SpotifyApi(credentials);
+    // Use the spotify package to create credentials. This is only needed for Search
+    var credentials = SpotifyApiCredentials(clientId, clientSecret);
+    spotifyWebApi = SpotifyApi(credentials);
 
-      return spotifyWebApi != null;
-    } else {
-      
-      var result = await SpotifySdk.connectToSpotifyRemote(
+    var c = await spotifyWebApi.getCredentials();
+    authenticationToken = c.accessToken;
+
+    // Use the spotify_sdk package if on mobile AND HOST
+    if (!kIsWeb) {
+      await SpotifySdk.connectToSpotifyRemote(
         clientId: this.clientId,
         redirectUrl: this.redirectUri);
-
-      // Get Authentication Token for Search
-      authenticationToken = await SpotifySdk.getAuthenticationToken(
-        clientId: this.clientId,
-        redirectUrl: this.redirectUri);
-
-      return result != null && authenticationToken != null;
     }
+
+    return authenticationToken != null;
   }
 
   /// Pause a [Song]]
   @override
   Future<void> pause() async {
     await SpotifySdk.pause();
-    // try {
-    //   await SpotifySdk.pause();
-    // } on PlatformException catch (e) {
-
-    // } on MissingPluginException {
-
-    // }
   }
 
   /// Play a [Song] given a [uri]
   @override
   Future<void> playTrack(String uri) async {
     await SpotifySdk.play(spotifyUri: uri);
-    // try {
-    //   await SpotifySdk.play(spotifyUri: uri);
-    // } on PlatformException catch (e) {
-
-    // } on MissingPluginException {
-
-    // }
   }
 
   /// Resume the [Song] playing right now
   @override
   Future<void> resume() async {
     await SpotifySdk.resume();
-    // try {
-    //   await SpotifySdk.resume();
-    // } on PlatformException catch (e) {
-
-    // } on MissingPluginException {
-
-    // }
   }
 
   /// Use the [authenticationToken] to search a query using the Spotify Web API
   @override
   Future<List<Song>> search(String query) async {
-    if (kIsWeb) {
-      return searchWeb(query);
-    } else {
-      return searchSDK(query);
-    }
-  }
-
-  Future<List<Song>> searchSDK(String query) async {
-    List<Song> searchResults = List();
-
-    String search = 'https://api.spotify.com/v1/search/?type=track&market=US&q=';
-    search += formatQuery(query);
-
-    var response = await http.get(search, headers: {
-      'Authorization': 'Bearer ${this.authenticationToken}'
-    });
-
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-
-      searchResults = getSongs(jsonResponse);
-    }
-
-    return searchResults;
-  }
-
-  Future<List<Song>> searchWeb(String query) async {
     List<Song> searchResults = List();
 
     var search = await spotifyWebApi.search
       .get(query)
-      .first(2)
+      .first()
       .catchError((err) => print((err as SpotifyException).message));
 
     if (search != null) {
@@ -161,26 +107,4 @@ class Spotify extends Service {
 
     return searchResults;
   }
-
-  /// Formats a query by converting spaces to `%20` for [http] requests
-  String formatQuery(String query) {
-    return query.replaceAll(' ', '%20');
-  }
-
-  /// Parses the [jsonResponse] from an [http] request to produce a List of [Song] objects
-  List<Song> getSongs(var jsonRespone) {
-    List<Song> songs = List();
-
-    for (var item in jsonRespone['tracks']['items']) {
-      String id = item['id'];
-      String uri = item['uri'];
-      String trackName = item['name'];
-      String artist = item['artists'][0]['name'];
-      Service service = this;
-
-      songs.add(Song(id, uri, trackName, artist, service));
-    }
-
-    return songs;
-  } 
 }
