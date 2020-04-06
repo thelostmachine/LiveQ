c5d53333ee3130faff89266b0b3085ac9b3443ce
andrewywong <ndrew.wong@gmail.com>
2020-04-06 02:00:51 -0500

    Add confirmation to exit room

--- liveq/lib/pages/room.dart
+++ liveq/lib/pages/room.dart
@@ -54,66 +54,91 @@ class _RoomState extends State<Room> {
   Widget build(BuildContext context) {
     final double _radius = 25.0;
     // final RoomArguments args = ModalRoute.of(context).settings.arguments;
-    return Scaffold(
-        appBar: AppBar(
-          // title: Text(args.roomName),
-          title: Text('Room Name'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.music_note),
-              onPressed: () => Navigator.pushNamed(context, '/connect_services')
-                  .then((didConnect) {
-                if (didConnect) {
-                  setState(() {
-                    Player.isConnected = true;
-                  });
-                }
-              }),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () => _searchSong(context),
+    return WillPopScope(
+      onWillPop: () => _onWillPop(),
+      child: Scaffold(
+          appBar: AppBar(
+            // title: Text(args.roomName),
+            title: Text('Room Name'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.music_note),
+                onPressed: () =>
+                    Navigator.pushNamed(context, '/connect_services')
+                        .then((didConnect) {
+                  if (didConnect) {
+                    setState(() {
+                      Player.isConnected = true;
+                    });
+                  }
+                }),
               ),
-            ),
-          ],
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: _queueListView(context),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(_radius),
-                    topRight: Radius.circular(_radius),
-                  ),
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    stops: [
-                      0.0,
-                      0.7,
-                    ],
-                    colors: [
-                      Color(0xFF47ACE1),
-                      Color(0xFFDF5F9D),
-                    ],
-                  ),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () => _searchSong(context),
                 ),
-                child: (Player.isConnected)
-                    ? _musicPlayer(context)
-                    : _connectionStatus(context), //MusicPlayer(),
               ),
-            ),
-          ],
-        ));
+            ],
+          ),
+          body: Column(
+            children: <Widget>[
+              Expanded(
+                child: _queueListView(context),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(_radius),
+                      topRight: Radius.circular(_radius),
+                    ),
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: [
+                        0.0,
+                        0.7,
+                      ],
+                      colors: [
+                        Color(0xFF47ACE1),
+                        Color(0xFFDF5F9D),
+                      ],
+                    ),
+                  ),
+                  child: (Player.isConnected)
+                      ? _musicPlayer(context)
+                      : _connectionStatus(context), //MusicPlayer(),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Are you sure'),
+            content: const Text('Do you want to exit?'),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('No'),
+              ),
+              FlatButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: const Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
   }
 
   void _searchSong(BuildContext context) async {
