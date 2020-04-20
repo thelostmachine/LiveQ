7ceae91ca6b4ea8587efaa96a948d71c475b07c3
Bear Ergo <bjergo@email.tamu.edu>
2020-04-20 16:49:47 -0500

    Update ApplePlayer.dart
    need to test

--- liveq/lib/utils/ApplePlayer.dart
+++ liveq/lib/utils/ApplePlayer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_sound/flutter_sound.dart';
 import 'dart:async';
-import 'dart:io';
 import 'package:intl/intl.dart' show DateFormat;
 import 'package:intl/date_symbol_data_local.dart';
 
@@ -31,6 +30,57 @@ class ApplePlayer {
     String path = await flutterSound.startPlayer(uri);
     await flutterSound.setVolume(1.0);
 
+
+    try {
+      _playerSubscription = flutterSound.onPlayerStateChanged.listen((e) async {
+        if (e != null) {
+          slider_current_position = e.currentPosition;
+          max_duration = e.duration;
+
+          final remaining = e.duration - e.currentPosition;
+
+          DateTime date = DateTime.fromMillisecondsSinceEpoch(
+              e.currentPosition.toInt(),
+              isUtc: true);
+
+          DateTime endDate = DateTime.fromMillisecondsSinceEpoch(
+              remaining.toInt(),
+              isUtc: true);
+
+          String startText = DateFormat('mm:ss', 'en_GB').format(date);
+          String endText = DateFormat('mm:ss', 'en_GB').format(endDate);
+
+         
+            
+              this._startText = startText;
+              this._endText = endText;
+              this.slider_current_position = slider_current_position;
+              this.max_duration = max_duration;
+            
+          
+        } else {
+          slider_current_position = 0;
+
+          if (_playerSubscription != null) {
+            _playerSubscription.cancel();
+            _playerSubscription = null;
+          }
+          
+            this._isPlaying = false;
+            this._startText = '00:00';
+            this._endText = '00:00';
+          
+        }
+      });
+    } catch (err) {
+      print('error: $err');
+      
+        this._isPlaying = false;
+      
+    
+
+
+  }
   }
 
   applePausePlayer() async {
