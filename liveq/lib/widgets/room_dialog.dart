eba14b79d3561d6e6550f55053a2aeb73e845275
andrewywong <ndrew.wong@gmail.com>
2020-04-28 21:35:44 -0500

    Revert overflow change

--- liveq/lib/widgets/room_dialog.dart
+++ liveq/lib/widgets/room_dialog.dart
@@ -148,33 +148,3 @@ Future<void> createRoomDialog(
     },
   );
 }
-
-class KeyboardAvoiding extends StatelessWidget {
-  final Widget child;
-  final Curve curve;
-  final Duration duration;
-  final double kFactor;
-
-  KeyboardAvoiding({
-    this.child,
-    this.curve = Curves.easeInOut,
-    this.duration = const Duration(milliseconds: 200),
-    this.kFactor = 1.0,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final _verticalOffset = MediaQuery.of(context).viewInsets.bottom * -kFactor;
-
-    return AnimatedContainer(
-      duration: duration,
-      curve: curve,
-      transform: Matrix4.translationValues(
-        0.0,
-        _verticalOffset,
-        0.0,
-      ),
-      child: child,
-    );
-  }
-}
